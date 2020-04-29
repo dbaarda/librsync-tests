b4001bcb360023cdb366c40dc35510de608a9c8f
Donovan Baarda <abo@minkirri.apana.org.au>
2020-04-29 12:44:46 +1000

    Include peformance tests at 70% hashtable loadfactor.
    Update testall.sh to include sizes just before/after 70% hashtable loadfactor.
    
    Regenerate all peformance data and graphs.

--- testfull.sh
+++ testfull.sh
@@ -6,8 +6,8 @@
 
 sigargs="$@"
 
-# These sizes pick out the sawtooth at the hashtable's 80% full mark.
-fsizes="32K 51K 52K 64K 102K 103K 128K 204K 205K 256K 409K 410K 512K 819K 820K 1024K"
+# These sizes pick out the sawtooth at the hashtable's 70% and 80% full mark.
+fsizes="32K 44K 45K 51K 52K 64K 89K 90K 102K 103K 128K 179K 180K 204K 205K 256K 358K 359K 409K 410K 512K 716K 717K 819K 820K 1024K"
 
 echo "Testing rdiff with signature args '${sigargs}'"
 echo "==============================================="
