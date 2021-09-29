0e0c2fa58360207491bf451413ab7289c20510e9
Donovan Baarda <abo@minkirri.apana.org.au>
2021-09-29 16:18:08 +1000

    Add -Rrollsum and -S-1 runs to testvers.sh.
    These runs are necessary for graphs to generate.

--- testvers.sh
+++ testvers.sh
@@ -14,4 +14,14 @@ for version in $versions; do
   buildversion $version
   runfull $version
   runfull $version -b1024 -S8
+  # Use rollsum for versions that support rabinkarp
+  if [[ "$version" > "v2.1.0" || "$version" == "master" ]]; then
+    runfull $version -Rrollsum
+    runfull $version -b1024 -S8 -Rrollsum
+  fi
+  # Use min strongsum size for versions that support it.
+  if [[ "$version" > "v2.2.1" || "$version" == "master" ]]; then
+    runfull $version -S-1
+    runfull $version -b1024 -S-1
+  fi
 done
